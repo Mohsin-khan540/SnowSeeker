361f0219c22193725288af14367df6bd5fce3845
Mohsin-khan540 <arhamk4555@gmail.com>
2025-12-06 22:26:57 +0500

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Mohsin khan on 06/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
